6be4b5e26025ec184cec14832cac7b7772e32068
Matthew <158123486+Machu549@users.noreply.github.com>
2024-12-27 14:50:33 -0700

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -1,9 +1,44 @@
 // This is the code for a text editor tutorial
+/*
+ * kilo.c
+ * 
+ * Copyright 2024  <pi@picluster>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
 
-int main (){
-  char c;
-  while (read(STDIN_FILENO, &c, 1) ==1);
-  return 0;
+void enableRawMode(){
+	struct termios raw;
+	tcgetattr(STDIN_FILENO, &raw);
+	raw.c_lflag &= ~(ECHO);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main()
+{
+	enableRawMode();
+	
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	return 0;
 }
